191e0aded96c732732d33d4f2facd215e1c5eeb2
habib <habibguesmi@gmail.com>
2025-05-29 00:32:57 +0200

    fix cors origin

---
+++ src/main/java/com/example/userapi/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.example.userapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("https://habibguesmi.github.io",
+                                "http://localhost:4200",
+                                "http://sql7.freesqldatabase.com"
+                                )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
+
